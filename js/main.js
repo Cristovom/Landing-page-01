78b87bec5ac0e27bd550ce7534c593acbc48206a
Cristovom Nunes <cristovom007@gmail.com>
2022-07-11 23:14:07 -0300

    update nav

--- js/main.js
+++ js/main.js
@@ -10,20 +10,6 @@ $(".menu-hamburguer").on('click', function() {
 //   typeSpeed: 50
 // });
 
-// COLOCANDO AS PALAVRAS AQUI
-// var typedr = new Typed('.element', {
-//   strings: [
-//     'Essas são as palavras que irão aparecer',
-//     'First sentence.',
-//     'Second sentence.',
-//     'Have a great day!',
-//     'dsdsfgfd',
-//   ],
-//   stringsElement: null,
-//   typeSpeed: 50,
-//   loop: true
-// });
-
 var wordsfooter = new Typed('.words', {
   strings: [
     'Aqui são as palavras onde irão no footer',
@@ -43,31 +29,25 @@ const parceiros = new Swiper('.group-parceiros', {
   effect: 'cards',
   centerInsufficientSlides: true,
   centeredSlides: true,
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // }
 });
 
 //Instagram na home
-// token cris === IGQVJYU2o1cWVwdnZAKdEJESjRSQjJ3OWV3Y2VOaGFYdlJTeFN1UC1SXzdERmM5ZAFZAyZAkwzWmNtcHAtTldST2JUYmpzNFZA1NWR3NFFidDVOYUhLb0ZABTU9JaWlNTVZAoX08ySGxPV1l5MXZAONVpfRVJQVAZDZD
-// token arthur === IGQVJXdjJxOHJ6MGl6eFZARRWZAuWGJVX3hXeS0zN3FyNkdWQURRNFFRNFRXMnN5dXk2N3h6Sy1faWhISW1ITWw4U0FXcVFXV2dIQ0NMT1NSYkR0QkVEZAm9zbk5CY1RyV3BNYi1ESDZAnbDRuYzJzTUFoUAZDZD
-$.ajax({
-    type:"GET",
-    url: "https://graph.instagram.com/me/media?fields=media_type,media_url,id,caption,permalink&limit=10&access_token=IGQVJXdjJxOHJ6MGl6eFZARRWZAuWGJVX3hXeS0zN3FyNkdWQURRNFFRNFRXMnN5dXk2N3h6Sy1faWhISW1ITWw4U0FXcVFXV2dIQ0NMT1NSYkR0QkVEZAm9zbk5CY1RyV3BNYi1ESDZAnbDRuYzJzTUFoUAZDZD",
-    success: function(data){
-    console.log(data);
-    for( x in data.data ){
-        if(data.data[x].media_type == 'VIDEO'){
-          $('#instagramfeed').append('<a target="_blank" href="'+data.data[x].permalink+'"><video autoplay muted="true" loop="true" src="'+data.data[x].media_url+'"></video></a>');
-        }
-        else{
-          $('#instagramfeed .group-photo').append('<a target="_blank" href="'+data.data[x].permalink+'" class="card-unico" style="background-image:url('+data.data[x].media_url+')"></a>');
-        }
-    }
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-    console.log(data);
-  },
-  dataType: "jsonp"
-});
+// $.ajax({
+//     type:"GET",
+//     url: "https://graph.instagram.com/me/media?fields=media_type,media_url,id,caption,permalink&limit=10&access_token=ddddd",
+//     success: function(data){
+//     console.log(data);
+//     for( x in data.data ){
+//         if(data.data[x].media_type == 'VIDEO'){
+//           $('#instagramfeed').append('<a target="_blank" href="'+data.data[x].permalink+'"><video autoplay muted="true" loop="true" src="'+data.data[x].media_url+'"></video></a>');
+//         }
+//         else{
+//           $('#instagramfeed .group-photo').append('<a target="_blank" href="'+data.data[x].permalink+'" class="card-unico" style="background-image:url('+data.data[x].media_url+')"></a>');
+//         }
+//     }
+//   },
+//   error: function(jqXHR, textStatus, errorThrown) {
+//     console.log(data);
+//   },
+//   dataType: "jsonp"
+// });
